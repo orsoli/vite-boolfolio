08c7f9527d1fe225d575a57d9e336682de1139e9
orsoli <fljcoli@gmail.com>
2024-11-14 17:55:51 +0100

    imorted and implemented axios to get results based on endpoint api

--- src/components/partials/Main.vue
+++ src/components/partials/Main.vue
@@ -1,17 +1,31 @@
 <script>
+// Import 
+import axios from 'axios';
+
 export default {
     data() {
         return {
-          
+            // Variables
+            projectsUrl: 'http://127.0.0.1:8000/api/projects', // Save endpoint api
         };
     },
 
-    components: {
-
+    methods: {
+        getProjectsResults(){
+            axios.get(this.projectsUrl).then((response) => {
+             console.log(response)
+            })
+                .catch((error) => {
+                    console.log(error); // Print errors in console
+                })
+                .finally(() => {
+                    console.log("Geting api Projects results is finished") // Print message after api riturn results
+                });
+        }
     },
 
-    methods: {
-
+    mounted(){
+        this.getProjectsResults();
     }
 };
 </script>
