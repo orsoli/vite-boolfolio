23d8140e917bf3c22276dd78a31c24e376d6b486
orsoli <fljcoli@gmail.com>
2024-11-14 17:38:00 +0100

    Created Header Main and Footer vue and implementet in App.vue

---
+++ src/components/partials/Footer.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+
+    },
+
+    methods: {
+
+    }
+};
+</script>
+
+<template>
+    <h1 class="text-center bg-body-secondary p-2">FOOTER</h1>
+</template>
+
+<style lang='scss' scoped></style>
---
+++ src/components/partials/Header.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+
+    },
+
+    methods: {
+
+    }
+};
+</script>
+
+<template>
+    <h1 class="text-center bg-light p-2">Header</h1>
+</template>
+
+<style lang='scss' scoped>
+</style>
